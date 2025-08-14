28bd071a397856669e757ed3d789e4db6b6380bf
mrjinow <mohajiinowaza09@gmail.com>
2025-08-14 09:35:41 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import Doctors from "./Pages/Doctors";
 import Header from "./Pages/Header";
 
 function App(){
   return <div>
     <Header />
-    <Doctors />
+   
   </div>
 }
 export default App
