4e50c1ad5d71b94093ed9546f9013c9235e52abd
mrjinow <mohajiinowaza09@gmail.com>
2025-08-15 08:27:19 +0300

    resp

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+
 import Doctors from "./Pages/Doctors";
 import Header from "./Pages/Header";
+import V1 from "./profilles/V1";
 
 function App(){
   return <div>
     <Header />
- <Doctors />
+    <Doctors />
+   <V1 />
   </div>
 }
 export default App
